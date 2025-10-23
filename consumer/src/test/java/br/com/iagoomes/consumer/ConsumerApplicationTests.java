920fc7f76e42d3d6e776ca30d995287e6d4192a7
Iago Gomes <iagoomes@outlook.com>
2025-10-23 20:37:33 -0300

    feat: init project

---
+++ consumer/src/test/java/br/com/iagoomes/consumer/ConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.iagoomes.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
